2585ed7ffcfc9776c3652fd4aca4b65fd1bb6ef2
vasudevaraogullipalli <vasudev.gullipalli@gmail.com>
2024-04-27 22:49:38 +0530

    Add files via upload
    Day1

---
+++ Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	 float browserVersion = 100.2f; //(use dataType float) 
+	String browserName = "firefox"; //(use dataType String)
+	boolean isVisible = true; //(use dataType boolean)
+	int releaseYear = 1998; //(use dataType int)
+	char browserLogo = 'f'; //(use datatype char)
+
+	System.out.println("use dataType float: "+browserVersion);
+	System.out.println("use dataType String: "+browserName);
+	System.out.println("use dataType boolean: "+isVisible);
+	System.out.println("use dataType int: "+releaseYear);
+	System.out.println("use datatype char: "+browserLogo);
+	
+	
+
+}
+}
